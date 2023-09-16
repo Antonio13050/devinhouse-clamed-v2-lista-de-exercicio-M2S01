61894bfdcf5dcb91596dc09a6011c78bd6677da6
Antonio Carlos Virissimo <antonio13050@gmail.com>
2023-09-16 16:08:12 -0700

    Ex 02 - Calculadora IMC

--- src/main/java/exerc2/Exerc2.java
+++ src/main/java/exerc2/Exerc2.java
@@ -1,7 +1,22 @@
 package exerc2;
 
+import java.util.Scanner;
+
 public class Exerc2 {
     public static void main(String[] args) {
-        
+
+        Scanner scanner= new Scanner(System.in);
+
+        System.out.println("Digite a sua altura:");
+        double altura = Double.parseDouble(scanner.nextLine());
+
+        System.out.println("Digite o seu peso:");
+        double peso = Double.parseDouble(scanner.nextLine());
+
+        double imc = peso / (altura * altura);
+
+        System.out.printf("Seu IMC é: %2f", imc);
+
+
     }
 }
